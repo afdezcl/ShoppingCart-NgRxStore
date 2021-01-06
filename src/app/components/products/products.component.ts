9353a3f5917cbfe2d1f168c39b1dd605554f5b5a
afdezcl <adrifdez_@uma.es>
2021-01-06 15:42:05 +0100

    Service to get products created

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/products/product.service';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private productService: ProductService
+  ) { }
 
   ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts()
+      .subscribe(response => console.log(response));
   }
 
 }
---
+++ src/app/services/products/product.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  getProducts() {
+    return this.httpClient.get(`${ environment.apiBaseUrl }/products`);
+  }
+
+}
